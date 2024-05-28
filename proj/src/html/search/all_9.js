1f9c014013df633118aae7ac315dec1b52f5e20e
tiago <up202008790@up.pt>
2024-05-28 16:34:38 +0100

    changes in doxygen

--- proj/src/html/search/all_9.js
+++ proj/src/html/search/all_9.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['read_5fbuffer_0',['read_buffer',['../mouse_8h.html#a3044dc4155ea758b6a7b7f6e4abe3696',1,'mouse.c']]],
-  ['rotate_5fpiece_1',['rotate_piece',['../game_8h.html#a11526869630884b8348c7070d017b129',1,'game.c']]]
+  ['reset_5fscreen_1',['reset_screen',['../game_8h.html#aea3523527d3e994366375c5c1d864c25',1,'game.c']]],
+  ['rotate_5fpiece_2',['rotate_piece',['../game_8h.html#a11526869630884b8348c7070d017b129',1,'game.c']]]
 ];
--- proj/src/html/search/functions_5.js
+++ proj/src/html/search/functions_5.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['is_5finput_5fbuffer_5ffull_0',['is_input_buffer_full',['../mouse_8h.html#a9b2558009e1b213a1c8eefba7ccf0c1d',1,'mouse.c']]],
   ['is_5fline_5ffull_1',['is_line_full',['../game_8h.html#ad3c94caed8f855de91e9a9eb851d7752',1,'game.c']]],
-  ['is_5fpiece_5fat_5fbottom_2',['is_piece_at_bottom',['../game_8h.html#a51a5d0e17b976f95a88d1f26ae311454',1,'game.c']]]
+  ['is_5fpiece_5fat_5fbottom_2',['is_piece_at_bottom',['../game_8h.html#a51a5d0e17b976f95a88d1f26ae311454',1,'game.c']]],
+  ['is_5fwithin_5frectangle_3',['is_within_rectangle',['../game_8h.html#a36de923cf2a8193cca60033ee74381db',1,'game.c']]]
 ];
